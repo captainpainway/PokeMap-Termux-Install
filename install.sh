e878d506b8462a9174d7d3e436104acc7a93b2c7
captainpainway <mary.c.staggs@gmail.com>
2016-07-20 00:19:06 -0400

    Install error

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt install gcc -y
 pip2 install flask
 git clone https://github.com/AHAAAAAAA/PokemonGo-Map.git
 cd PokemonGo-Map
-pip2 install --upgrade -r requirements.txt
+pip2 install -r requirements.txt
 rm -r /data/data/com.termux/files/usr/bin/pokemap
 ln -s /data/data/com.termux/files/home/PokeMap-Termux-Install/pokemap-login-create.sh /data/data/com.termux/files/usr/bin/pokemap
 pokemap
