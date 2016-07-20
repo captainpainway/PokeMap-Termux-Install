cd9bf8875a2d68e75683df87cf4cb5eefcbe7a61
captainpainway <mary.c.staggs@gmail.com>
2016-07-19 23:43:48 -0400

    Update

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 apt install python2 -y
 apt install gcc -y
 pip2 install flask
+git clone https://github.com/AHAAAAAAA/PokemonGo-Map.git
 cd PokemonGo-Map
 pip2 install --upgrade -r requirements.txt
-git clone https://github.com/AHAAAAAAA/PokemonGo-Map.git
 rm -r /data/data/com.termux/files/usr/bin/pokemap
 ln -s /data/data/com.termux/files/home/PokeMap-Termux-Install/pokemap-login-create.sh /data/data/com.termux/files/usr/bin/pokemap
 pokemap
